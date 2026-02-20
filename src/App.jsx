5a12d5c9587b8c8f9a9fa533f83f78d7b037e5c4
jagnyadattadcs <jagnyadatta.dcs@gmail.com>
2026-02-20 10:26:34 +0530

    Initial project setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-purple-600 text-4xl text-center font-bold'>Employee Management System</h1>
+    </>
+  )
+}
+
+export default App
